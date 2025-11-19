5cacc2158c5680b2e93aaf12e58d9c02890a00c2
sagnikBag <146710246+Sagnikrick568@users.noreply.github.com>
2025-11-20 02:05:29 +0530

    instagram

--- 45day cohort/script.js
+++ 45day cohort/script.js
@@ -1,26 +1,29 @@
 let btn = document.querySelector("button");
 let h2 = document.querySelector("h2");
-var inner = document.querySelector(".inner")
-let grow = 0
+let inner = document.querySelector(".inner");
+let grow = 0;
 
 
 btn.addEventListener("click",function(){
+
   btn.style.pointerEvents = "none"
+  let num = 50 + Math.floor(Math.random()*50)
+console.log("dowload completed in`${ num/10}` sec");
 
-  let num = Math.floor( Math.random()*50)+50
-
-
-   let int = setInterval(() => {
-    grow++
-    h2.innerHTML =  grow + "%";
-    inner.style.width = grow + "%"
-    }, num);
+ let int = setInterval(() => {
+  grow++
+  h2.innerHTML = grow + "%";
+  inner.style.width = grow + "%";
 
 
-    setTimeout(() => {
-      clearInterval(int)
-        btn.innerHTML = "khatam"
-        btn.style.opacity = 0.5
-    }, num*100);
+}, num);
+
+setTimeout(() => {
+  clearInterval(int)
+  btn.innerText = 'sesh hoegace'
+  btn.style.opacity = 0.5;
+}, num*100);
+  
 
 })
+
