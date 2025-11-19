23d8e4d73d1e9a7d081b344aa13cb3d789aed826
sagnikBag <146710246+Sagnikrick568@users.noreply.github.com>
2025-11-19 14:51:14 +0530

    setinterval

--- 45day cohort/script.js
+++ 45day cohort/script.js
@@ -1,14 +1,26 @@
-let btn = document.getElementById("button");
-let h2 = document.getElementById("h2");
-let inner = document.getElementsByClassName("inner")
+let btn = document.querySelector("button");
+let h2 = document.querySelector("h2");
+var inner = document.querySelector(".inner")
 let grow = 0
 
 
-btn.addEventListener("click", function(){
- let x = Math.random()*100;
- setInterval(function(){
-grow++
-  h2.innerText = grow + "%" 
- })
+btn.addEventListener("click",function(){
+  btn.style.pointerEvents = "none"
 
-},1000)
+  let num = Math.floor( Math.random()*50)+50
+
+
+   let int = setInterval(() => {
+    grow++
+    h2.innerHTML =  grow + "%";
+    inner.style.width = grow + "%"
+    }, num);
+
+
+    setTimeout(() => {
+      clearInterval(int)
+        btn.innerHTML = "khatam"
+        btn.style.opacity = 0.5
+    }, num*100);
+
+})
