c67dadc0776a769941c10526e93ed0d234758c46
sagnikBag <146710246+Sagnikrick568@users.noreply.github.com>
2025-11-23 00:40:35 +0530

    piano

--- 48day cohort/script.js
+++ 48day cohort/script.js
@@ -24,73 +24,125 @@ keys.forEach(key =>{
    const audio = sound[note];
    audio.currentTime = 0
    audio.play()
+   key.classList.add("active");
+    setTimeout(() => {
+    key.classList.remove("active");
+  }, 150);
    
  })
 })
-document.addEventListener("keydown",function(e){
- let audio = ""
- if(e.key.toLowerCase() == "q"){
-  audio = sound["C"]
-  audio.currentTime = 0
-   audio.play()
- }
- else if(e.key.toLowerCase() == "w"){
-  audio = sound["C#"]
-  audio.currentTime = 0
-   audio.play()
- }
- else if(e.key.toLowerCase() == "e"){
-  audio = sound["D"]
-  audio.currentTime = 0
-   audio.play()
- }
- else if(e.key.toLowerCase() == "r"){
-  audio = sound["D#"]
-  audio.currentTime = 0
-   audio.play()
- }
- else if(e.key.toLowerCase() == "t"){
-  audio = sound["E"]
-  audio.currentTime = 0
-   audio.play()
- }
- else if(e.key.toLowerCase() == "y"){
-  audio = sound["F"]
-  audio.currentTime = 0
-   audio.play()
- }
- else if(e.key.toLowerCase() == "a"){
-  audio = sound["F#"]
-  audio.currentTime = 0
-   audio.play()
- }
- else if(e.key.toLowerCase() == "s"){
-  audio = sound["G"]
-  audio.currentTime = 0
-   audio.play()
- }
- else if(e.key.toLowerCase() == "d"){
-  audio = sound["G#"]
-  audio.currentTime = 0
-   audio.play()
- }
- else if(e.key.toLowerCase() == "f" || "n"){
-  audio = sound["A"]
-  audio.currentTime = 0
-   audio.play()
- }
- else if(e.key.toLowerCase() == "g"){
-  audio = sound["A#"]
-  audio.currentTime = 0
-   audio.play()
- }
- else if(e.key.toLowerCase() == "h"){
-  audio = sound["B"]
-  audio.currentTime = 0
-   audio.play()
- }
- else{
-  console.log("wrong press");
+// document.addEventListener("keydown",function(e){
+//  let audio = ""
+//  if(e.key.toLowerCase() == "q"){
+//   audio = sound["C"]
+//   audio.currentTime = 0
+//    audio.play()
+//    .classList.add("active")
+
   
+//  }
+//  else if(e.key.toLowerCase() == "w"){
+//   audio = sound["C#"]
+//   audio.currentTime = 0
+//    audio.play()
+   
+//  }
+//  else if(e.key.toLowerCase() == "e"){
+//   audio = sound["D"]
+//   audio.currentTime = 0
+//    audio.play()
+   
+//  }
+//  else if(e.key.toLowerCase() == "r"){
+//   audio = sound["D#"]
+//   audio.currentTime = 0
+//    audio.play()
+   
+//  }
+//  else if(e.key.toLowerCase() == "t"){
+//   audio = sound["E"]
+//   audio.currentTime = 0
+//    audio.play()
+   
+//  }
+//  else if(e.key.toLowerCase() == "y"){
+//   audio = sound["F"]
+//   audio.currentTime = 0
+//    audio.play()
+   
+//  }
+//  else if(e.key.toLowerCase() == "a"){
+//   audio = sound["F#"]
+//   audio.currentTime = 0
+//    audio.play()
+   
+//  }
+//  else if(e.key.toLowerCase() == "s"){
+//   audio = sound["G"]
+//   audio.currentTime = 0
+//    audio.play()
+   
+//  }
+//  else if(e.key.toLowerCase() == "d"){
+//   audio = sound["G#"]
+//   audio.currentTime = 0
+//    audio.play()
+   
+//  }
+//  else if(e.key.toLowerCase() == "f" || "n"){
+//   audio = sound["A"]
+//   audio.currentTime = 0
+//    audio.play()
+   
+//  }
+//  else if(e.key.toLowerCase() == "g"){
+//   audio = sound["A#"]
+//   audio.currentTime = 0
+//    audio.play()
+   
+//  }
+//  else if(e.key.toLowerCase() == "h"){
+//   audio = sound["B"]
+//   audio.currentTime = 0
+//    audio.play()
+   
+//  }
+//  else{
+//   console.log("wrong press");
+  
+//  }
+// })
+document.addEventListener("keydown",function(e){
+ let map = {
+  "q": "C",
+  "w": "C#",
+  "e": "D",
+  "r": "D#",
+  "t": "E",
+  "y": "F",
+  "a": "F#",
+  "s": "G",
+  "d": "G#",
+  "f": "A",
+  "g": "A#",
+  "h": "B"
+};
+let pressed = e.key.toLowerCase();
+
+ if(map[pressed]){
+  let note =map[pressed];
+  let audio = sound[note];
+
+  let keyDiv = document.getElementById(note);
+  audio.currentTime = 0;
+  audio.play();
+
+  keyDiv.classList.add("active");
+
+  setTimeout(() => {
+      keyDiv.classList.remove("active");
+  }, 150);
+
+
  }
 })
