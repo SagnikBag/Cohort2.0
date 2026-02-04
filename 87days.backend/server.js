963fa551e4d77e66d78c5fa1380b6fbdcd61e19d
sagnikBag <146710246+Sagnikrick568@users.noreply.github.com>
2026-02-05 02:24:06 +0530

    day2

--- 87days.backend/server.js
+++ 87days.backend/server.js
@@ -1,15 +1,10 @@
-const express = require("express")
-const app = express()
-app.use(express.json())
+const app = require("./src/app")
+const connectToDb = require("./config/database")
 
-const notes = []
-app.post("/notes",(req,res)=>{
- console.log(req.body);
- 
- res.send('note created')
-})
+
+connectToDb()
 
 app.listen(3000,()=>{
- console.log("Server is running on port 3000");
- 
+  console.log("server running on port 3000");
+  
 })
