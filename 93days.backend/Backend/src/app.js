8e1e06e2086395e9c98e42dca521b89b3fbd92e4
sagnikBag <146710246+Sagnikrick568@users.noreply.github.com>
2026-02-09 16:10:18 +0530

    fullstack deplyment

--- 93days.backend/Backend/src/app.js
+++ 93days.backend/Backend/src/app.js
@@ -8,7 +8,7 @@ const path = require("path");
  const app = express()
  app.use(express.json());
  app.use(cors());
- app.use(express.static("../public"))
+ app.use(express.static("./public"))
 
  app.post('/api/notes', async (req,res)=>{
   const {description,age,title} = req.body
