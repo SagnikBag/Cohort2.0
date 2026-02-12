d0fc899c23b6677961fa6cd4786358f41a72e81e
sagnikBag <sagnikbag2001@gmail.com>
2026-02-12 16:31:53 +0530

    insta clone

--- 101days.backend/src/config/database.js
+++ 101days.backend/src/config/database.js
@@ -1,1 +1,10 @@
 const mongoose = require("mongoose")
+
+async function connectToDb() {
+  await mongoose.connect(process.env.MONGO_URI)
+
+  console.log("Connected to Db");
+  
+}
+
+module.exports = connectToDb
