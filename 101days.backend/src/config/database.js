3b4b9eb233dace8d249e10bb9937c9a43e286746
sagnikBag <sagnikbag2001@gmail.com>
2026-02-12 20:22:52 +0530

    git

--- 101days.backend/src/config/database.js
+++ 101days.backend/src/config/database.js
@@ -1,10 +1,5 @@
 const mongoose = require("mongoose")
 
-async function connectToDb() {
-  await mongoose.connect(process.env.MONGO_URI)
-
-  console.log("Connected to Db");
-  
+async function connectToDb(){
+ 
 }
-
-module.exports = connectToDb
