b91af87265807184352a5f2d90d6bd3fc7814cad
sagnikBag <sagnikbag2001@gmail.com>
2026-02-12 13:41:31 +0530

    finally

--- 98days.backend/src/routes/auth.route.js
+++ 98days.backend/src/routes/auth.route.js
@@ -1,44 +1,43 @@
-const express = require("express")
-const userModel = require("../models/user.model")
-const jwt = require("jsonwebtoken")
-const authRouter = express.Router()
-const crypto = require("crypto")
+const express = require("express");
+const userModel = require("../models/user.model");
+const jwt = require("jsonwebtoken");
+const authRouter = express.Router();
+const crypto = require("crypto");
 
-authRouter.post("/register", async(req,res)=>{
- const {email,name,password} = req.body
+authRouter.post("/register", async (req, res) => {
+  const { email, name, password } = req.body;
 
- const isUserAlreadyExists = await userModel.findOne({email})
+  const isUserAlreadyExists = await userModel.findOne({ email });
 
- if(isUserAlreadyExists){
-  return res.status(200).json({
-   message:"User  email already exists"
-  })
- }
- const hash = crypto.createHash("md5").update(password).digest("hex")
+  if (isUserAlreadyExists) {
+    return res.status(200).json({
+      message: "User  email already exists",
+    });
+  }
+  const hash = crypto.createHash("md5").update(password).digest("hex");
 
- const user = await userModel.create({
-  email, name, password:hash
- })
+  const user = await userModel.create({
+    email,
+    name,
+    password: hash,
+  });
 
-const token = jwt.sign({
- id: user._id,
- email:user.email
-},
-process.env.JWT_SECRET
-)
+  const token = jwt.sign(
+    {
+      id: user._id,
+      email: user.email,
+    },
+    process.env.JWT_SECRET,
+  );
 
-res.cookie("jwt_token",token)
+  res.cookie("jwt_token", token);
 
-
-
- res.status(201).json({
-  message:"user registered",
-  user,
-  token
- })
-})
-
-
+  res.status(201).json({
+    message: "user registered",
+    user,
+    token,
+  });
+});
 
 authRouter.post("/login", async (req, res) => {
   const { email, password } = req.body;
@@ -74,41 +73,5 @@ authRouter.post("/login", async (req, res) => {
     user,
   });
 });
-<<<<<<< HEAD:98days.backend/src/routes/auth.route.js
 
-authRouter.post("/login", async (req, res) => {
-
- const { email, password } = req.body
-
- const user = await userModel.findOne({ email })
-
- if (!user) {
-   return res.status(404).json({ message: "user not found" })
- }
-
- const isPasswordMatched =
-   user.password === crypto.createHash("md5").update(password).digest("hex")
-
- if (!isPasswordMatched) {
-   return res.status(401).json({ message: "invalid password" })
- }
-
- const token = jwt.sign(
-   { id: user._id },
-   process.env.JWT_SECRET
- )
-
- res.cookie("jwt_token", token)
-
- res.status(200).json({
-   message: "user logged in port 3000 sagnik ",
-   user
- })
-})
-// sagnik
-
-module.exports = authRouter
-=======
-module.exports = authRouter
-
->>>>>>> d162126e075f437b82470827ce10ccb8244b3524:97days.backend - Copy/src/routes/auth.route.js
+module.exports = authRouter;
