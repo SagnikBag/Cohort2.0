b552915bb3216fb6eafa305d27b6bde3627c71c0
sagnikBag <sagnikbag2001@gmail.com>
2026-02-12 13:37:40 +0530

    change in database.js folder

--- 98days.backend/src/config/database.js
+++ 98days.backend/src/config/database.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose")
 function connectToDb(){
  mongoose.connect(process.env.MONGO_URI)
  .then(()=>{
-  console.log('connect to db on port 3000');
+  console.log('connect to Database');
   
  })
 }
