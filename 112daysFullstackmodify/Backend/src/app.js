ff1169d41229d704c310044d84d5fe5c90400cf5
sagnikBag <sagnikbag2001@gmail.com>
2026-03-16 01:17:56 +0530

    moodify

--- 112daysFullstackmodify/Backend/src/app.js
+++ 112daysFullstackmodify/Backend/src/app.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const cookieParser = require("cookie-parser")
 const cors = require("cors")
+const path = require("path")
 
 const app = express();
 app.use(express.json());
@@ -8,7 +9,8 @@ app.use(cookieParser());
 app.use(cors({
  origin:"http://localhost:5173",
  credentials: true
-}))
+}));
+app.use(express.static("./public"))
 
 const authRoutes = require("./routes/auth.routes")
 const songRoutes =require("./routes/song.routes")
