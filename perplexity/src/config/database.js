fb02cbd110e95f257fd9c639b2244edd13d030fc
sagnikBag <sagnikbag2001@gmail.com>
2026-03-27 23:27:50 +0530

    mongodb in connected

--- perplexity/src/config/database.js
+++ perplexity/src/config/database.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectToDb = async()=>{
  const conn = await mongoose.connect(process.env.MONGO_URI);
- console.log(`MongoDb connected: ${conn.connection.host}`);
+ console.log(`MongoDb connect: ${conn.connection.host}`);
  
 };
 
