283d5207b001a9585613521a1173747d5bca3243
sagnikBag <sagnikbag2001@gmail.com>
2026-03-29 02:15:08 +0530

    Add email service and validation for user registration; update package dependencies

--- perplexity/src/routes/auth.routes.js
+++ perplexity/src/routes/auth.routes.js
@@ -1,5 +1,12 @@
 import { Router } from "express";
+import { register } from "../controllers/auth.controller";
+import { registerValidator } from "../validators/auth.validator";
 
 const authRouter = Router();
 
-authRouter.post()
+
+// Register route
+// POST /api/auth/register
+// Request body: { username, email, password }
+// 
+authRouter.post("/reister",registerValidator,register);
