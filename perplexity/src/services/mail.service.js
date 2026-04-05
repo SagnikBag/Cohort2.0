073f4a3d314a5170dcb3af99cd5663d2ebdb9029
sagnikBag <sagnikbag2001@gmail.com>
2026-04-06 02:32:28 +0530

    Update mail service authentication method to use OAuth2

--- perplexity/src/services/mail.service.js
+++ perplexity/src/services/mail.service.js
@@ -4,8 +4,11 @@ import nodemailer from "nodemailer";
 const transporter = nodemailer.createTransport({
  service:"gmail",
  auth:{
+  type:'OAuth2',
   user:process.env.GOOGLE_USER,
-  pass:process.env.GOOGLE_APP_PASSWORD
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  refreshToken: process.env.GOOGLE_REFRESH_TOKEN,
+  clientId: process.env.GOOGLE_CLIENT_ID
  }
 })
 
