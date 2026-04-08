d897f79d0e3223b122f90c913887add400d0974f
sagnikBag <sagnikbag2001@gmail.com>
2026-04-08 17:24:43 +0530

    Fix cookie parser middleware initialization by adding parentheses

--- perplexity/src/app.js
+++ perplexity/src/app.js
@@ -7,7 +7,7 @@ const app = express()
 //middleware
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
-app.use(cookieParser)
+app.use(cookieParser())
 
 //api check
 app.get("/",(req,res)=>{
